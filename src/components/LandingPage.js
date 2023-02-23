c91436ba9a8220adfa6c0e7144cd7e87dc70f640
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-02-23 15:34:52 -0800

    add function component and placeholder text

---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <>
+      <div>LandingPage Placeholder</div>
+      <h3>Greeting with button to enter site (todoList.js)</h3>
+    </>
+  )
+}
+
+export default LandingPage
