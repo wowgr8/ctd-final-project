b4d080b0cbeedfd101e21eff907881e64da4f6e8
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-02-23 17:40:41 -0800

    add tableName prop, add useEffect to handle getting airtable data upon first render. add isLoading ternary operator to display TodoList or Loading... text

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,10 +1,39 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import TodoList from './TodoList';
 
-function TodoContainer() {
-  const [todoList, setTodoList] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+function TodoContainer({ tableName }) {
+  const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`, {
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`
+      }
+    })
+    .then((response) => response.json())
+    .then((result) => {
+      setTodoList(result.records);
+      console.log(result.records)
+      setIsLoading(false)
+    })
+  },[tableName]);
+  
+  useEffect(() => {
+    if( isLoading === false ){
+      localStorage.setItem("todoList", JSON.stringify(todoList));
+    }
+  }, [todoList]);
+
   return (
-    <div>TodoContainer Placeholder</div>
+    <div>
+      <p>TodoContainer Placeholder!</p>
+      <h1>This is the table name: {tableName}</h1>
+      {isLoading 
+        ? (<p>Loading...</p>)
+        : (<TodoList todoList={todoList}  />)
+      }
+    </div>
   )
 }
 
