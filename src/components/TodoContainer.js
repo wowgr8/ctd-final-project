68cbc7b14a6615a76a68cd9d16a23b4901addfc0
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-02-23 16:04:22 -0800

    add useState variables: todoList and isLoading.

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function TodoContainer() {
+  const [todoList, setTodoList] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
   return (
     <div>TodoContainer Placeholder</div>
   )
