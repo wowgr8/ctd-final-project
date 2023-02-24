a284cd732f9f57ebd116f598ccd1fd8539cd7ac6
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-02-24 15:42:51 -0800

    add todoList prop and temporary loop through todoList - WIP

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
-function TodoList() {
+function TodoList({ todoList }) {
+  console.log("In todoList.js - todoList: ", todoList)
   return (
-    <div>TodoList Placeholder</div>
+    <>
+      <div>TodoList Placeholder</div>
+      {todoList.map((item) =>{
+        return <h1>{item.fields.Title}</h1>
+      })}
+    </>
   )
 }
 
