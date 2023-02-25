625e677bcd745671f0a44a83ebe12da50a053fe6
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-02-25 13:13:06 -0800

    add onRemoveTodo as prop and passed into TodoListItem

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import TodoListItem from './TodoListItem'
 
-function TodoList({ todoList }) {
+function TodoList({ todoList, onRemoveTodo }) {
   console.log("In todoList.js - todoList: ", todoList)
   return (
     <>
       <div>TodoList.js Placeholder</div>
       {todoList.map((item) =>{
-        return <TodoListItem key={item.id} {...item}  />
+        return <TodoListItem key={item.id} {...item}  onRemoveTodo={onRemoveTodo}/>
       })}
     </>
   )
