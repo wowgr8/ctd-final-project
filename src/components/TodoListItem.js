c3d75cfac8fbdfd58ec430500028f6b30c3be7da
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-02-24 16:01:43 -0800

    add proptypes

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 function TodoListItem({ fields }) {
   return (
     <>
       <li >{fields.Title}</li>
-      
     </>
   )
+}
+
+TodoListItem.propTypes = {
+  fields: PropTypes.object,
 }
 
 export default TodoListItem
