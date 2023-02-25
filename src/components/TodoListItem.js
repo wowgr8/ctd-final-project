94f128a584e5f8a2fddb2ef4313ffcf94d55228c
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-02-25 13:14:01 -0800

    add onRemoveTodo and id as props, add button invoking onRemoveTodo

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function TodoListItem({ fields }) {
+function TodoListItem({ fields, onRemoveTodo, id }) {
   return (
     <>
       <li >{fields.Title}</li>
+      <button type="button" onClick={() => onRemoveTodo(id)} >
+        Remove
+      </button>
     </>
   )
 }
