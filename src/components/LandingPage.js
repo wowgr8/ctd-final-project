81ad27c9c320000fbedf860e0e840f826b28587a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-02-25 21:05:19 -0800

    add button to enter todolist container. add useNavigate to onEnter() to redirect to todoContainer

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { useNavigate } from "react-router-dom";
+import TodoContainer from './TodoContainer';
+
+
 
 function LandingPage() {
+  const [entered, isEntered] = useState(false)
+  
+  let navigate = useNavigate(); 
+  
+  const onEnter = () => {
+    isEntered(!entered)
+    console.log("Entered is:", entered)
+    navigate('/todoContainer')
+    console.log("navigate")
+  }
   return (
     <>
       <div>LandingPage Placeholder</div>
-      <h3>Greeting with button to enter site (todoList.js)</h3>
+      <h3>Greeting with button to enter site (todoContainer.js)</h3>
+      <button onClick={onEnter} >
+        Click Here To Get Started
+      </button>
     </>
   )
 }
