cae0cd09337c1d795c8c4d416c5c51372c2d105a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-02-28 13:28:23 -0800

    remove commented out code

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -4,39 +4,8 @@ import styles from './TodoListItem.module.css';
 
 function TodoListItem({ fields, onRemoveTodo, id }) {
   return (
-    // <>
-    //   <li >{fields.Title}</li>
-    //   <button type="button" onClick={() => onRemoveTodo(id)} >
-    //     Remove
-    //   </button>
-    // </>
-
-    // <table>
-    //   <tr>
-    //     <td>{fields.Title}</td>
-    //     <td>{fields.Description}</td>
-    //     <td>{fields.Status}</td>
-    //     <td>{fields["Due Date"]}</td>
-    //     <td>
-    //       <button type="button" onClick={() => onRemoveTodo(id)}>
-    //         Remove
-    //       </button>
-    //     </td>
-    //   </tr>
-    // </table>
-
-    // <table>
-    //   <thead>
-    //     <tr>
-    //       <th>Title</th>
-    //       <th>Description</th>
-    //       <th>Status</th>
-    //       <th>Due Date</th>
-    //       <th>Actions</th>
-    //     </tr>
-    //   </thead>
-      <tbody>
-        <tr>
+    <tbody>
+      <tr>
         <td>{fields.Title}</td>
         <td>{fields.Description}</td>
         <td>{fields.Status}</td>
@@ -47,9 +16,7 @@ function TodoListItem({ fields, onRemoveTodo, id }) {
           </button>
         </td>
       </tr>
-      </tbody>
-    // </table>
-
+    </tbody>
   )
 }
 
