a20bd675e0b8fa05bb4a06fa93358dd305bde627
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-02-27 22:16:27 -0800

    add temporary table body - WIP

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './TodoListItem.module.css';
 
 function TodoListItem({ fields, onRemoveTodo, id }) {
   return (
-    <>
-      <li >{fields.Title}</li>
-      <button type="button" onClick={() => onRemoveTodo(id)} >
-        Remove
-      </button>
-    </>
+    // <>
+    //   <li >{fields.Title}</li>
+    //   <button type="button" onClick={() => onRemoveTodo(id)} >
+    //     Remove
+    //   </button>
+    // </>
+
+    // <table>
+    //   <tr>
+    //     <td>{fields.Title}</td>
+    //     <td>{fields.Description}</td>
+    //     <td>{fields.Status}</td>
+    //     <td>{fields["Due Date"]}</td>
+    //     <td>
+    //       <button type="button" onClick={() => onRemoveTodo(id)}>
+    //         Remove
+    //       </button>
+    //     </td>
+    //   </tr>
+    // </table>
+
+    // <table>
+    //   <thead>
+    //     <tr>
+    //       <th>Title</th>
+    //       <th>Description</th>
+    //       <th>Status</th>
+    //       <th>Due Date</th>
+    //       <th>Actions</th>
+    //     </tr>
+    //   </thead>
+      <tbody>
+        <tr>
+        <td>{fields.Title}</td>
+        <td>{fields.Description}</td>
+        <td>{fields.Status}</td>
+        <td>{fields["Due Date"]}</td>
+        <td>
+          <button type="button" onClick={() => onRemoveTodo(id)}>
+            Remove
+          </button>
+        </td>
+      </tr>
+      </tbody>
+    // </table>
+
   )
 }
 
