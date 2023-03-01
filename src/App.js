0a27817eb6b739678edf1c8cb4b81525b50fdfe8
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-02-28 17:07:26 -0800

    remove placeholder text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   return (
     <div className={styles.bgColor}>
       <Router>
-        <p>Hello World!</p>
         <Routes>
           <Route path='/' exact element={<LandingPage />} />
           <Route path='todoContainer' exact element={<TodoContainer tableName="Your Todos" />} />
