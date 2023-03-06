2010007cba5405048e4558b35f15055e532f6eec
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-03-06 15:31:37 -0800

    add styling for each button and notes for Done and Edit button placeholders

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './TodoListItem.module.css';
 import { ReactComponent as GarbageCanSVG} from '../svg/garbage-bin-svgrepo-com.svg'
+import { ReactComponent as EditSVG} from '../svg/edit-4-svgrepo-com.svg'
+import { ReactComponent as DoneSVG} from '../svg/completed.svg'
 
 function TodoListItem({ fields, onRemoveTodo, id }) {
   return (
@@ -12,8 +14,16 @@ function TodoListItem({ fields, onRemoveTodo, id }) {
         <td>{fields.Status}</td>
         <td>{new Date(fields['Due Date']).toLocaleDateString()}</td>
         <td>
-          <button type="button" onClick={() => onRemoveTodo(id)}>
+          <button id={styles.trashBtn} type="button" onClick={() => onRemoveTodo(id)}>
             <GarbageCanSVG height="30px" width="30px"/>
+          </button>
+
+          {/* Edit and Done buttons below are non-functional. WIP */}
+          <button id={styles.editBtn} type="button" onClick={"fn() placeholder"}>
+            <EditSVG height="30px" width="30px"/>
+          </button>
+          <button id={styles.doneBtn} type="button" onClick={"fn() placeholder"}>
+            <DoneSVG height="30px" width="30px"/>
           </button>
         </td>
       </tr>
